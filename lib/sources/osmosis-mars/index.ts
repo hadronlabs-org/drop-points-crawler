4666e86f812f9ed3943a8a898614fbdd3924ee73
deniszagumennov <denis.zagumennov@hadronlabs.org>
2024-05-24 17:26:12 +0400

    Add osmosis-mars-source

--- lib/sources/osmosis-mars/index.ts
+++ lib/sources/osmosis-mars/index.ts
@@ -1,6 +1,7 @@
 import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
 import { queryContractOnHeight } from '../../query';
-import { parse } from 'path';
+import { CbOnUserBalances } from '../../../types/cbOnUserBalances';
+import pLimit from 'p-limit';
 
 interface PositionResponse {
   account_id: string;
@@ -19,6 +20,8 @@ interface PositionResponse {
   }[];
 }
 
+const MARS_DENOM =
+  'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2';
 const NFT_CONTRACT_ADDRESS = process.env.OSMOSIS_MARS_NFT_CONTRACT;
 if (!NFT_CONTRACT_ADDRESS) {
   throw new Error('OSMOSIS_MARS_NFT_CONTRACT environment variable not set');
@@ -41,61 +44,59 @@ const getClient = async () => {
   return client;
 };
 
-const DEFAULT_LIMIT = 100;
-const PARALLEL_QUERIES = parseInt(
-  process.env.OSMOSIS_MARS_CONCURRENT || '3',
+const PAGINATION_LIMIT = parseInt(
+  process.env.OSMOSIS_MARS_PAGINATION_LIMIT || '300',
+  10,
+);
+const CONCURRENCY_LIMIT = parseInt(
+  process.env.OSMOSIS_MARS_CONCURRENCY_LIMIT || '3',
   10,
 );
 
-const getAccounts = async (
+const getAccountTokens = async (
   client: Tendermint34Client,
   height: number,
-  limit = DEFAULT_LIMIT,
-  start_after?: string,
+  limit: number,
+  startAfter?: string,
 ): Promise<string[]> => {
   const data = await queryContractOnHeight<{ tokens: string[] }>(
     client,
     NFT_CONTRACT_ADDRESS,
     height,
     {
-      all_tokens: { limit, start_after },
+      all_tokens: { limit, start_after: startAfter },
     },
   );
-  return data.tokens as string[];
+  return data.tokens;
 };
 
-const getAllAccounts = async (
+const getAccountTokensOwned = async (
   client: Tendermint34Client,
   height: number,
-  limit = DEFAULT_LIMIT,
+  owner: string,
 ): Promise<string[]> => {
-  const allTokens: string[] = [];
-  while (true) {
-    const tokens = await getAccounts(
-      client,
-      height,
-      DEFAULT_LIMIT,
-      allTokens.length ? allTokens[allTokens.length - 1] : undefined,
-    );
-    allTokens.push(...tokens);
-    if (tokens.length < DEFAULT_LIMIT) {
-      break;
-    }
-  }
-  return allTokens;
+  const data = await queryContractOnHeight<{ tokens: string[] }>(
+    client,
+    NFT_CONTRACT_ADDRESS,
+    height,
+    {
+      tokens: { owner },
+    },
+  );
+  return data.tokens;
 };
 
 const getAccountPositions = async (
   client: Tendermint34Client,
   height: number,
-  token: string,
+  account: string,
 ): Promise<PositionResponse> => {
   const data = await queryContractOnHeight<PositionResponse>(
     client,
     CREDIT_CONTRACT,
     height,
     {
-      positions: { account_id: token },
+      positions: { account_id: account },
     },
   );
   return data;
@@ -104,35 +105,119 @@ const getAccountPositions = async (
 const getAccountOwner = async (
   client: Tendermint34Client,
   height: number,
-  token: string,
+  account: string,
 ): Promise<string> => {
   const data = await queryContractOnHeight<{ owner: string }>(
     client,
     NFT_CONTRACT_ADDRESS,
     height,
     {
-      owner_of: { token_id: token },
+      owner_of: { token_id: account },
     },
   );
+
   return data.owner;
 };
 
-export const getLastBlockHeight = async (): Promise<number> => {
+const getLastBlockHeight = async (): Promise<number> => {
   const client = await getClient();
   const status = await client.status();
   return status.syncInfo.latestBlockHeight;
 };
 
-export const getPrice = (): number => 1;
+const getBalanceOrNull = (positions: PositionResponse) => {
+  if (positions.debts.length > 0 && positions.lends.length > 0) {
+    const foundAsset = positions.lends.find((lend) => lend.denom === MARS_DENOM);
+    if (foundAsset) return foundAsset.amount;
+  }
 
-const getAccountInfo = async (
+  return null;
+};
+
+const getAddressAndBalance = async (
   client: Tendermint34Client,
   height: number,
-  token: string,
-): Promise<{ owner: string; positions: PositionResponse }> => {
-  const [owner, positions] = await Promise.all([
-    getAccountOwner(client, height, token),
-    getAccountPositions(client, height, token),
-  ]);
-  return { owner, positions };
+  accountToken: string,
+): Promise<{ address: string; balance: string } | null> => {
+  const owner = await getAccountOwner(client, height, accountToken);
+
+  const accountTokensOwned = await getAccountTokensOwned(client, height, owner);
+
+  if (accountTokensOwned.length === 1) {
+    const positions = await getAccountPositions(client, height, accountToken);
+    const positionsBalance = getBalanceOrNull(positions);
+    return positionsBalance
+      ? { address: owner, balance: positionsBalance }
+      : null;
+  }
+
+  if (accountToken !== accountTokensOwned[0]) return null;
+
+  let aggregatedBalance = 0;
+  let hasFittingPosition = false;
+
+  accountTokensOwned.forEach(async (accountToken) => {
+    const currentPositions = await getAccountPositions(
+      client,
+      height,
+      accountToken,
+    );
+    const balance = getBalanceOrNull(currentPositions);
+
+    if (balance && !hasFittingPosition) hasFittingPosition = true;
+
+    aggregatedBalance += balance ? parseInt(balance, 10) : 0;
+  });
+
+  return hasFittingPosition
+    ? { address: owner, balance: String(aggregatedBalance) }
+    : null;
 };
+
+const getPrice = (): number => 1;
+
+const getUsersBalances = async (
+  height: number,
+  cb: CbOnUserBalances,
+): Promise<void> => {
+  const client = await getClient();
+
+  const limit = PAGINATION_LIMIT;
+  let startAfter = undefined;
+  let accountTokens: string[] = [];
+
+  while (true) {
+    startAfter =
+      accountTokens.length > 0
+        ? accountTokens[accountTokens.length - 1]
+        : undefined;
+    accountTokens = await getAccountTokens(client, height, limit, startAfter);
+    if (accountTokens.length === 0) break;
+
+    const withConcurrencyLimit = pLimit(CONCURRENCY_LIMIT);
+    const settledResults = await Promise.allSettled(
+      accountTokens.map((accountToken) =>
+        withConcurrencyLimit(
+          async () => await getAddressAndBalance(client, height, accountToken),
+        ),
+      ),
+    );
+
+    cb(
+      settledResults.reduce(
+        (
+          filteredResult: { address: string; balance: string }[],
+          settledResult,
+        ) => {
+          if (settledResult.status === 'fulfilled' && settledResult.value) {
+            filteredResult.push(settledResult.value);
+          }
+          return filteredResult;
+        },
+        [],
+      ),
+    );
+  }
+};
+
+export { getLastBlockHeight, getPrice, getUsersBalances };
