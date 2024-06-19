9d03ee89d00b17a87c43a258dc7022b8f2a10960
Sergey Ratiashvili <serg.s.r@gmail.com>
2024-06-19 16:43:45 +0200

    feat: prom export

---
+++ server/prometeus.ts
@@ -0,0 +1,43 @@
+import type Database from 'bun:sqlite';
+import * as client from 'prom-client';
+
+export const getRegistry = (
+  config: any,
+  db: Database,
+): client.Registry<'text/plain; version=0.0.4; charset=utf-8'> => {
+  const gauges = ['points', 'points_l1', 'points_l2'].map(
+    (field) =>
+      new client.Gauge({
+        name: field,
+        help: `${field} value for an address`,
+        labelNames: ['address'],
+        collect() {
+          const data = db
+            .query<
+              {
+                points: string;
+                address: string;
+              },
+              null
+            >(
+              `SELECT ${field} points, address FROM user_points_public WHERE address IN ('${config.watched_addresses.join("','")}')`,
+            )
+            .all(null);
+          data.forEach((row) => {
+            this.set({ address: row.address }, parseFloat(row.points));
+          });
+        },
+      }),
+  );
+
+  const register = new client.Registry();
+
+  client.collectDefaultMetrics({
+    register: register,
+  });
+
+  for (const gauge of gauges) {
+    register.registerMetric(gauge);
+  }
+  return register;
+};
