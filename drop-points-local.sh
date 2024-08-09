371b61998f349419c015df1c736821fadeee1c7a
deniszagumennov <denis.zagumennov@hadronlabs.org>
2024-08-09 21:24:14 +0400

    Some prelaunch fixes

--- drop-points-local.sh
+++ drop-points-local.sh
@@ -1,10 +1,14 @@
-docker exec drop-points-crawler bun crawl prepare
-docker exec drop-points-crawler bun crawl crawl neutron
-docker exec drop-points-crawler bun crawl finish
+docker exec mainnet-drop-points-crawler bun crawl prepare
+docker exec mainnet-drop-points-crawler bun crawl crawl neutron
+docker exec mainnet-drop-points-crawler bun crawl crawl neutron-astroport
+docker exec mainnet-drop-points-crawler bun crawl crawl neutron-astroport-generator
+docker exec mainnet-drop-points-crawler bun crawl finish
 
-docker exec drop-points-crawler ./backup.sh backups
-mkdir -p "$(pwd)/backups"
-mkdir -p "$(pwd)/tmp/backups"
-docker cp drop-points-crawler:/usr/src/app/backups/. "$(pwd)/tmp/backups"
-rsync -av --ignore-existing "$(pwd)/tmp/backups/" "$(pwd)/backups/"
-rm -rf "$(pwd)/tmp/backups"
+docker exec mainnet-drop-points-crawler ./backup.sh backups
+mkdir -p "$(pwd)/mainnet-backups"
+mkdir -p "$(pwd)/tmp/mainnet-backups"
+docker cp mainnet-drop-points-crawler:/usr/src/app/backups/. "$(pwd)/tmp/mainnet-backups"
+cp "$(pwd)/tmp/mainnet-backups/$(ls -1 "$(pwd)/tmp/mainnet-backups/" | tail -n 1)" "$(pwd)/mainnet-backups/"
+aws s3 cp "$(pwd)/mainnet-backups/$(ls -1 "$(pwd)/mainnet-backups" | tail -n 1)" s3://crawler.backup/
+rm -rf "$(pwd)/tmp"
+docker exec mainnet-drop-points-crawler rm -rf backups
