a3863ddcb55f4a0bcd5bd0c13419577df57c219e
deniszagumennov <denis.zagumennov@hadronlabs.org>
2024-11-27 15:35:46 +0400

    Simplified pseudo random

--- crawler/index.ts
+++ crawler/index.ts
@@ -132,7 +132,7 @@ program
 
     const timeShift = // same for all protocols bc of IBC and stuff
       config.random && config.random === 'pseudo'
-        ? await getPseudoRandom(config.protocols['neutron']?.rpc)
+        ? getPseudoRandom(batchId)
         : getTrueRandom();
 
     for (const protocol of protocolsInDb) {
--- crawler/random.ts
+++ crawler/random.ts
@@ -1,29 +1,17 @@
-import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
 import seedrandom from 'seedrandom';
+import * as os from 'os';
+import * as crypto from 'crypto';
 
 const getTrueRandom = () => Math.random();
 
-const uint8ArrayToHex = (array: Uint8Array): string =>
-  Array.from(array)
-    .map((byte) => byte.toString(16).padStart(2, '0'))
-    .join('');
+const getPseudoRandom = (batchId: number) => {
+  const hostname = os.hostname();
+  const seed = crypto
+    .createHash('sha256')
+    .update(`${hostname}_${batchId}`)
+    .digest('hex');
 
-const getPseudoRandom = async (neutronRpc: string) => {
-  if (!neutronRpc) {
-    throw new Error('neutron RPC is not specified!');
-  }
-
-  const client = await Tendermint34Client.connect(neutronRpc);
-  const status = await client.status();
-
-  const blockHeight = Math.floor(status.syncInfo.latestBlockHeight / 100) * 100;
-  const block = await client.block(blockHeight);
-  const blockHash = block?.blockId?.hash;
-  const hexHash = blockHash ? uint8ArrayToHex(blockHash) : undefined;
-
-  client.disconnect();
-
-  const rng = seedrandom(hexHash);
+  const rng = seedrandom(seed);
   return rng();
 };
 
