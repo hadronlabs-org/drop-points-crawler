af6b38a28b376e1c806132f5531668743e41bc48
Sergey Ratiashvili <serg.s.r@gmail.com>
2025-01-09 21:08:12 +0100

    feat: etherium network link

---
+++ lib/link/index.ts
@@ -0,0 +1,41 @@
+import Database from 'bun:sqlite';
+import { Logger } from 'pino';
+
+export const insertLinkRecord = (
+  db: Database,
+  logger: Logger,
+  localAddress: string,
+  remoteAddress: string,
+  network: string,
+): boolean => {
+  const ts = (Date.now() / 1000) | 0;
+
+  logger.debug(
+    'Insert Link for local address %s remote address [%s] %s',
+    localAddress,
+    network,
+    remoteAddress,
+  );
+
+  try {
+    const userKycTx = db.prepare(
+      'INSERT INTO user_network_link (network, local_address, remote_address, ts) VALUES (?, ?, ?, ?)',
+    );
+    userKycTx.run(network, localAddress, remoteAddress, ts);
+    userKycTx.finalize();
+  } catch (e) {
+    logger.error('Unexpected error occurred: %s', (e as Error).message);
+    throw new Error('Unexpected error occurred', {
+      cause: 'INTERNAL_SERVER_ERROR',
+    });
+  }
+
+  logger.debug(
+    'Inserting link for address %s [%s] %s is finished',
+    remoteAddress,
+    network,
+    localAddress,
+  );
+
+  return true;
+};
---
+++ server/controllers/postLink/index.ts
@@ -0,0 +1,61 @@
+import { Database } from 'bun:sqlite';
+import { Logger } from 'pino';
+import { TRPCError } from '@trpc/server';
+import {
+  tRPCPostLinkRequest,
+  tRPCPostLinkResponse,
+} from '../../../types/tRPC/tRPCPostLink';
+import { verifyMessage } from 'viem';
+import { insertLinkRecord } from '../../../lib/link';
+import { TRPC_ERROR_CODE_KEY } from '@trpc/server/rpc';
+
+function isHexString(value: string): value is `0x${string}` {
+  return value.startsWith('0x');
+}
+
+const postLink =
+  (db: Database, logger: Logger) =>
+  async (req: tRPCPostLinkRequest): Promise<tRPCPostLinkResponse> => {
+    const {
+      input: { localAddress, remoteAddress, network, sign },
+    } = req;
+
+    if (network !== 'etherium') {
+      throw new TRPCError({
+        code: 'CONFLICT',
+        message: 'Invalid network',
+      });
+    }
+
+    if (!isHexString(sign) || !isHexString(remoteAddress)) {
+      throw new TRPCError({
+        code: 'CONFLICT',
+        message: 'Invalid address or signature',
+      });
+    }
+
+    const verified = await verifyMessage({
+      message: localAddress,
+      signature: sign,
+      address: remoteAddress,
+    });
+
+    if (!verified) {
+      throw new TRPCError({
+        code: 'CONFLICT',
+        message: 'Invalid signature',
+      });
+    }
+
+    try {
+      insertLinkRecord(db, logger, localAddress, remoteAddress, network);
+    } catch (e) {
+      throw new TRPCError({
+        code: (e as Error).cause as TRPC_ERROR_CODE_KEY,
+        message: (e as Error).message,
+      });
+    }
+    return true;
+  };
+
+export { postLink };
