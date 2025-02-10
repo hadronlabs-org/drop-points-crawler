548612c77e10c55e5a5d5a0aad6abed206617db1
Sergey Ratiashvili <serg.s.r@gmail.com>
2025-02-10 15:06:26 +0100

    chore: - image

--- db/index.ts
+++ db/index.ts
@@ -87,7 +87,7 @@ export const connect = (
       'CREATE TABLE IF NOT EXISTS kvstore (key TEXT PRIMARY KEY, value TEXT);',
     );
     db.exec(
-      'CREATE TABLE IF NOT EXISTS badges (id TEXT PRIMARY KEY, name TEXT NOT NULL, description TEXT, image TEXT, mul NUMERIC NOT NULL, params TEXT);',
+      'CREATE TABLE IF NOT EXISTS badges (id TEXT PRIMARY KEY, name TEXT NOT NULL, description TEXT, mul NUMERIC NOT NULL, params TEXT);',
     );
     db.exec(
       'CREATE TABLE IF NOT EXISTS user_badges (address TEXT, badge_id TEXT, start NUMERIC NOT NULL, end NUMERIC NOT NULL, PRIMARY KEY(address));',
