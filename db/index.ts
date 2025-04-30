8525bd5f1e5a76021d7bf684910ee1e40286ca02
deniszagumennov <denis.zagumennov@hadronlabs.org>
2025-04-30 18:54:59 +0400

    Add database storage

--- db/index.ts
+++ db/index.ts
@@ -100,6 +100,9 @@ export const connect = (
       );
     `);
     db.exec(
+      'CREATE TABLE IF NOT EXISTS users_archive (address TEXT, source TEXT, ts NUMERIC, PRIMARY KEY(address, source));',
+    );
+    db.exec(
       `CREATE UNIQUE INDEX IF NOT EXISTS user_network_link_network_IDX ON user_network_link (network,remote_address,local_address);`,
     );
     db.exec(
