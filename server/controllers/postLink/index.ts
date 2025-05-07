1d2a773ce3354132b932d7e05702bf3e76763b62
deniszagumennov <denis.zagumennov@hadronlabs.org>
2025-05-07 18:29:37 +0400

    Added initia linking

--- server/controllers/postLink/index.ts
+++ server/controllers/postLink/index.ts
@@ -8,11 +8,55 @@ import {
 import { verifyMessage } from 'viem';
 import { insertLinkRecord } from '../../../lib/link';
 import { TRPC_ERROR_CODE_KEY } from '@trpc/server/rpc';
+import { toHexAddress } from '../../../lib/initia-address';
 
 const ETHEREUM_NETWORK = 'ethereum';
+const INITIA_NETWORK = 'initia';
 
 function isHexString(value: string): value is `0x${string}` {
   return value.startsWith('0x');
+}
+
+function isInitiaString(value: string): boolean {
+  return value.startsWith('init');
+}
+
+function validateEthereumAddress(
+  localAddress: string,
+  remoteAddress: string,
+  sign: string,
+): Promise<boolean> {
+  if (!isHexString(sign) || !isHexString(remoteAddress)) {
+    throw new TRPCError({
+      code: 'CONFLICT',
+      message: 'Invalid address or signature',
+    });
+  }
+
+  return verifyMessage({
+    message: localAddress,
+    signature: sign,
+    address: remoteAddress,
+  });
+}
+
+function validateInitiaAddress(
+  localAddress: string,
+  remoteAddress: string,
+  sign: string,
+): Promise<boolean> {
+  if (!isHexString(sign) || !isInitiaString(remoteAddress)) {
+    throw new TRPCError({
+      code: 'CONFLICT',
+      message: 'Invalid address or signature',
+    });
+  }
+
+  return verifyMessage({
+    message: localAddress,
+    signature: sign,
+    address: toHexAddress(remoteAddress),
+  });
 }
 
 const postLink =
@@ -22,25 +66,33 @@ const postLink =
       input: { localAddress, remoteAddress, network, sign },
     } = req;
 
-    if (network !== ETHEREUM_NETWORK) {
+    if (network !== ETHEREUM_NETWORK && network !== INITIA_NETWORK) {
       throw new TRPCError({
         code: 'CONFLICT',
         message: 'Invalid network',
       });
     }
 
-    if (!isHexString(sign) || !isHexString(remoteAddress)) {
-      throw new TRPCError({
-        code: 'CONFLICT',
-        message: 'Invalid address or signature',
-      });
-    }
+    let verified = false;
 
-    const verified = await verifyMessage({
-      message: localAddress,
-      signature: sign,
-      address: remoteAddress,
-    });
+    switch (network) {
+      case ETHEREUM_NETWORK:
+        verified = await validateEthereumAddress(
+          localAddress,
+          remoteAddress,
+          sign,
+        );
+        break;
+      case INITIA_NETWORK:
+        verified = await validateInitiaAddress(
+          localAddress,
+          remoteAddress,
+          sign,
+        );
+        break;
+      default:
+        throw new TRPCError({ code: 'CONFLICT', message: 'Invalid network' });
+    }
 
     if (!verified) {
       throw new TRPCError({
